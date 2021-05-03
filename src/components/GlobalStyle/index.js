41b7dab992e56a555444fccb72ae21c96abd727b
gabjcav <69957148+gabjcav@users.noreply.github.com>
2021-05-03 13:39:08 +0200

    Created home and profile containers

---
+++ src/components/GlobalStyle/index.js
@@ -0,0 +1,12 @@
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    :root{
+        --orange-background-color: #fbad50; 
+
+        --font-size: 1rem; 
+
+    }
+`
+
+export default GlobalStyle; 
